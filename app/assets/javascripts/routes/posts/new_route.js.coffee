ee6550f82ef888b43d8ab173ee830a4bf302fef9
Tony Miller <mcfiredrill@gmail.com>
2014-04-07 14:41:59 -0700

    can create new posts!!! :facepunch:

--- app/assets/javascripts/routes/posts/new_route.js.coffee
+++ app/assets/javascripts/routes/posts/new_route.js.coffee
@@ -1,1 +1,9 @@
 Fortchan12.PostsNewRoute = Ember.Route.extend
+  model: (params) ->
+    @get('store').createRecord 'post'
+
+  actions:
+    create: (post) ->
+      route = this
+      post.save().then ->
+        route.transitionTo('posts')
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,13 @@ class PostsController < ApplicationController
   def show
     respond_with Post.find(params[:id])
   end
+
+  def create
+    respond_with Post.create(post)
+  end
+
+  private
+  def post
+    params[:post].permit(:name, :body)
+  end
 end
