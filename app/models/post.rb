339bb35a225ec8ad8d026cd86f592f749473a526
Tony Miller <mcfiredrill@gmail.com>
2014-04-22 09:38:37 -0700

    add photo_or_post validation from forttree

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ActiveRecord::Base
   has_attached_file :photo, styles: { thumb: "300x300>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  validate :validates_photo_or_post
   belongs_to :topic
+
+  def validates_photo_or_post
+    if body.blank? && photo_file_name.blank?
+      errors.add(:leaf, "must have text or a picture, why would you want to make a
+        blank post? ;3")
+    end
+  end
 end
