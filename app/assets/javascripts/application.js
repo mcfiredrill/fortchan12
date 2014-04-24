3129bfbc5d31893797fdf91f45318251d1665036
Nik Nyby <niknyby@gmail.com>
2014-04-24 17:56:15 -0400

    Add first front-end qunit test and run it with testem
    To use testem:
      - npm install -g testem
      - bundle exec rails s
      - testem

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery_ujs
+//= require handlebars
+
+//= require ember
+//= require ember-data
+
+//= require vendor
+
+//= require_self
+
+//= require fortchan12
+
+// for more details see: http://emberjs.com/guides/application/
+window.Fortchan12 = Ember.Application.create();
