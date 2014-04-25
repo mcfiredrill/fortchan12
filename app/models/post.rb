ca6ef502ef4a5decf198d85bc6a6e58b8e6452cf
Nik Nyby <niknyby@gmail.com>
2014-04-25 12:51:23 -0400

    default photo url, and foliage note

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_attached_file :photo, styles: { thumb: "300x300>" }
+  has_attached_file :photo, styles: { thumb: "300x300>" }, default_url: ''
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   validate :validates_photo_or_post
   validates_presence_of :topic_id
@@ -7,6 +7,7 @@ class Post < ActiveRecord::Base
 
   def validates_photo_or_post
     if body.blank? && photo_file_name.blank?
+      # TODO: remove all foliage terminology leftover from fort-tree 1.
       errors.add(:leaf, "must have text or a picture, why would you want to make a
         blank post? ;3")
     end
