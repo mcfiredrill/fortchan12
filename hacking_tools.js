18301b7ef610b426f5c387da1cdfb3d67c18e50e
Nik Nyby <niknyby@gmail.com>
2014-05-29 23:29:33 -0400

    add hacking tools

---
+++ hacking_tools.js
@@ -0,0 +1,25 @@
+var makePost = function(text) {
+  var store = Fortchan12.__container__.lookup('store:main'); var post = store.createRecord('post');
+  post.setProperties({
+    name: 'silly souls',
+    body: text,
+    photo: null
+  });
+
+  var topic = store.createRecord('topic');
+  topic.get('posts').then(function(posts) {
+    posts.pushObject(post);
+  });
+
+  topic.save().then(function() {
+    post.set('topic', topic);
+    post.save();
+  })
+};
+
+var tools = {};
+var loop = function(text) {
+  tools.job = setInterval('makePost("'+text+'")',2000);
+  // clearInterval(tools.job);
+};
+
